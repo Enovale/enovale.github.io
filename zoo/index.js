b0d8ba4b2472a22c108d216002f84695876174c0
Enovale <elijahbansley@gmail.com>
2023-09-04 19:34:41 -0500

    Sprint A1 Lab 2

--- zoo/index.js
+++ zoo/index.js
@@ -1,3 +1,5 @@
+printSeparator("Sprint A1 Lab 1")
+
 var zooName = "Zoo"
 var zooLocation = "Death Valley"
 var zooFoundingYear = "2012"
@@ -5,3 +7,19 @@ var zooFoundingYear = "2012"
 console.log("Zoo name: " + zooName);
 console.log("Zoo location: " + zooLocation)
 console.log("Zoo Founding Year: " + zooFoundingYear)
+
+printSeparator("Sprint A1 Lab 2")
+
+var monkey = "George"
+var alligator = "Allen"
+var eagle = "Birdly"
+
+console.log(typeof monkey)
+console.log(typeof(alligator))
+console.log(typeof eagle)
+
+function printSeparator(name) {
+    console.log()
+    console.log("======== " + name + " ========")
+    console.log()
+}
