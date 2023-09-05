4ec002c59ac6e8bc5b74108743cec3b26b7d2d84
Enovale <elijahbansley@gmail.com>
2023-09-04 19:23:55 -0500

    move folder and add clarifying text

--- index.js
+++
@@ -1,7 +0,0 @@
-var zooName = "Zoo"
-var zooLocation = "Death Valley"
-var zooFoundingYear = "2012"
-
-console.log(zooName);
-console.log(zooLocation)
-console.log(zooFoundingYear)
---
+++ zoo/index.js
@@ -0,0 +1,7 @@
+var zooName = "Zoo"
+var zooLocation = "Death Valley"
+var zooFoundingYear = "2012"
+
+console.log("Zoo name: " + zooName);
+console.log("Zoo location: " + zooLocation)
+console.log("Zoo Founding Year: " + zooFoundingYear)
