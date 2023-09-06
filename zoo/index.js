2eba4c24c1817221763fbf05e32bfd431484532c
Enovale <elijahbansley@gmail.com>
2023-09-05 20:32:01 -0500

    Corrections from rubric checking

--- zoo/index.js
+++ zoo/index.js
@@ -1,9 +1,9 @@
 function sprintA1Lab1() {
     printSeparator("Sprint A1 Lab 1")
 
-    var zooName = "Zoo"
-    var zooLocation = "Death Valley"
-    var zooFoundingYear = "2012"
+    let zooName = "Zoo"
+    let zooLocation = "Death Valley"
+    let zooFoundingYear = "2012"
 
     console.log("Zoo name: " + zooName);
     console.log("Zoo location: " + zooLocation)
@@ -13,20 +13,25 @@ function sprintA1Lab1() {
 function sprintA1Lab2() {
     printSeparator("Sprint A1 Lab 2")
 
-    var monkey = "George"
-    var alligator = "Allen"
-    var eagle = "Birdly"
+    let monkey = "George"
+    let alligator = "Allen"
+    let eagle = "Birdly"
+
+    if (typeof monkey === 'string')
+    {
+        console.log("The monkey is a string")
+    }
 
     console.log(typeof monkey)
-    console.log(typeof (alligator))
+    console.log(typeof alligator)
     console.log(typeof eagle)
 }
 
 function sprintA1Lab3() {
     printSeparator("Sprint A1 Lab 3")
     
-    var input = document.getElementById("sprintA1Lab3Text").value
-    var num = Number(input)
+    let input = document.getElementById("sprintA1Lab3Text").value
+    let num = Number(input)
     
     if (num == 300) {
         console.log("The number is equal to 300")
@@ -38,12 +43,12 @@ function sprintA1Lab3() {
 function sprintA1Lab4() {
     printSeparator("Sprint A1 Lab 4")
 
-    var animalAge = 400
-    var humanAge = animalAge / 7
+    let animalAge = 400
+    let humanAge = animalAge / 7
     humanAge > 50 ? console.log("The animal is " + humanAge + " and is OLD") : console.log("The animal is " + humanAge + " and is YOUTHFUL")
 
     // It says I need a unary but it doesn't say where so here you go
-    var unary = 0
+    let unary = 0
     console.log(unary)
     unary++
     console.log(unary)
