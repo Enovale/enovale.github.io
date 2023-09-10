c6332744ed375f99c64d688e00327e7ddf60b8f0
Enovale <elijahbansley@gmail.com>
2023-09-10 00:13:33 -0500

    add a2zoo

---
+++ zoo/a2zoo/js/index.js
@@ -0,0 +1,67 @@
+let zoo = null
+
+function createZoo() {
+    // Log to console
+    console.log("Creating zoo...")
+
+    // Create zoo object
+    zoo = {
+        name: "Como Zoo",
+        capacity: 200,
+        numGuests: 150,
+        animals: [],
+    }
+
+    // Log to console
+    console.log(zoo)
+}
+
+function populateZoo() {
+    // Push to array
+    zoo.animals.push({
+        name: "Perry",
+        type: "Platypus",
+        age: 2,
+        gender: "Female",
+        weight: 3.2,
+        isPregnant: true
+    })
+
+    // Create object
+    zoo.animals.push({
+        name: "Harry",
+        type: "Hummingbird",
+        age: 2,
+        gender: "Male",
+        weight: 3.2,
+        isPregnant: false
+    })
+
+    // Create object
+    zoo.animals.push({
+        name: "Sherry",
+        type: "Shark",
+        age: 2,
+        gender: "Female",
+        weight: 852,
+        isPregnant: true
+    })
+
+    // Create object
+    zoo.animals.push({
+        name: "Cherry",
+        type: "Chimpanzee",
+        age: 2,
+        gender: "Female",
+        weight: 3.2,
+        isPregnant: true
+    })
+
+    // Log animals
+    zoo.animals.forEach(animal => {
+        console.log(animal)
+    });
+
+    // Log animals
+    console.log(zoo.animals)
+}
