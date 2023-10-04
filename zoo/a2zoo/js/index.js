9535e3bd1a162fe3d7ca9b319eeb36ca96c89cff
Ellie <enova@waifu.club>
2023-10-04 04:23:27 -0500

    Add a4 zoo

--- zoo/a2zoo/js/index.js
+++ zoo/a2zoo/js/index.js
@@ -3,6 +3,7 @@ let zoo = null
 let zooAnimalContainer = document.getElementById("zooAnimalContainer")
 let zooNameElement = document.getElementById("zooName")
 let zooCapacityElement = document.getElementById("zooCapacity")
+let zooAnimalCountElement = document.getElementById("zooAnimalCount")
 let zooGuestsElement = document.getElementById("zooGuests")
 
 let zooAnimalTable = document.getElementById("zooAnimalTable")
@@ -30,24 +31,8 @@ function createZoo() {
 function updateDisplay() {
     zooNameElement.innerText = zoo.name
     zooCapacityElement.innerText = zoo.capacity
+    zooAnimalCountElement.innerText = zoo.animals.length
     zooGuestsElement.innerText = zoo.numGuests
-
-    // Add an HTML representation of all animals.
-    zoo.animals.forEach(function (v, i, arr) {
-        let newAnimal = document.createElement("p")
-        newAnimal.innerText = `Name: ${v.name}\nType: ${v.type}\nAge: ${v.age}\nGender: ${v.gender}\nWeight: ${v.weight}\nIs Pregnant: ${v.isPregnant}`
-        zooAnimalContainer.appendChild(newAnimal)
-
-        // Update the animal table
-        let animalRow = createTableRow([v.name, v.type, v.age, v.gender, v.weight, v.isPregnant])
-        zooAnimalTable.appendChild(animalRow)
-
-        // Update the animal choice form
-        let animalChoice = document.createElement("option")
-        animalChoice.value = i.toString()
-        animalChoice.innerText = v.name
-        zooAnimalDropdown.appendChild(animalChoice)
-    })
 }
 
 // Dynamically create a table row with the strings provided
@@ -59,42 +44,63 @@ function createTableRow(dataArr) {
         row.appendChild(data)
     })
 
+    // Add a remove and edit button
+    let editBtnContainer = document.createElement("td")
+    let editBtn = document.createElement("button")
+    editBtn.innerText = "Edit"
+    editBtn.addEventListener("click", (event) => {
+        let index = Array.prototype.indexOf.call(zooAnimalTable.children, row) - 1
+        populateEditForm(index)
+    })
+    editBtnContainer.appendChild(editBtn)
+    row.appendChild(editBtnContainer)
+
+    let removeBtnContainer = document.createElement("td")
+    let removeBtn = document.createElement("button")
+    removeBtn.innerText = "Remove"
+    removeBtn.addEventListener("click", (event) => {
+        let index = Array.prototype.indexOf.call(zooAnimalTable.children, row) - 1
+        removeZooAnimal(index)
+    })
+    removeBtnContainer.appendChild(removeBtn)
+    row.appendChild(removeBtnContainer)
+
     return row
 }
 
 function populateZoo() {
     // Push to array
-    zoo.animals.push({
+    addZooAnimal({
         name: "Perry",
         type: "Platypus",
         age: 2,
         gender: "Female",
         weight: 3.2,
         isPregnant: true
-    })
+    }, true)
 
     // Create object
-    zoo.animals.push({
+    addZooAnimal({
         name: "Harry",
         type: "Hummingbird",
         age: 2,
         gender: "Male",
         weight: 3.2,
         isPregnant: false
-    })
+    }, true)
 
     // Create object
-    zoo.animals.push({
+    addZooAnimal({
         name: "Sherry",
         type: "Shark",
         age: 2,
         gender: "Female",
         weight: 852,
         isPregnant: true
-    })
+    }, true)
 
     // Create object
-    zoo.animals.push({
+    addZooAnimal({
         name: "Cherry",
         type: "Chimpanzee",
         age: 2,
@@ -104,13 +110,55 @@ function populateZoo() {
     })
 
     // Log animals
-    zoo.animals.forEach(animal => {
-        console.log(animal)
-    });
-
-    // Log animals
     console.log(zoo.animals)
 
+    updateDisplay()
+}
+
+// Add new animal on top
+function addZooAnimal(animal, deferUpdate = false) {
+    insertAnimal(animal, zoo.animals.length, deferUpdate)
+}
+
+// Insert a new animal at a specific index
+function insertAnimal(animal, index, deferUpdate = false) {
+    // Add animal and log it
+    zoo.animals.splice(index, 0, animal)
+    console.log(animal)
+
+    // Add Animal HTML
+    let newAnimal = document.createElement("p")
+    newAnimal.innerText = `Name: ${animal.name}\nType: ${animal.type}\nAge: ${animal.age}\nGender: ${animal.gender}\nWeight: ${animal.weight}\nIs Pregnant: ${animal.isPregnant}`
+    zooAnimalContainer.insertBefore(newAnimal, zooAnimalContainer.childNodes[index])
+
+    // Update the animal table
+    let animalRow = createTableRow([animal.name, animal.type, animal.age, animal.gender, animal.weight, animal.isPregnant])
+    zooAnimalTable.insertBefore(animalRow, zooAnimalTable.childNodes[index + 2])
+
+    // Update the animal choice form
+    let animalChoice = document.createElement("option")
+    animalChoice.value = zooAnimalDropdown.childElementCount.toString()
+    animalChoice.innerText = animal.name
+    zooAnimalDropdown.insertBefore(animalChoice, zooAnimalDropdown.childNodes[index])
+
+    // Update zoo display if we're told to do so
+    if (!deferUpdate)
+        updateDisplay()
+}
+
+// Remove existing animal and insert a new one to edit it.
+function editZooAnimal(index, animal) {
+    removeZooAnimal(index)
+    insertAnimal(animal, index)
+}
+
+// Remove animal nodes and update the DOM
+function removeZooAnimal(index) {
+    console.log(index)
+    zoo.animals.splice(index, 1)
+    zooAnimalContainer.children[index].remove()
+    zooAnimalTable.children[index + 1].remove()
+    zooAnimalDropdown.children[index].remove()
     updateDisplay()
 }
 
@@ -135,3 +183,84 @@ a4Form.addEventListener('submit', event => {
     formError.innerText = ""
     formThankYou.style = ""
 });
+
+let animalForm = document.getElementById('animalForm');
+let animalError = document.getElementById("animalError")
+
+// Prevent form but submitting and display the thank you message.
+animalForm.addEventListener('submit', event => {
+    // prevent the browser from submitting
+    event.preventDefault();
+
+    // Make sure the user put in a username
+    let name = animalForm.elements['animalName'].value
+    if (!name) {
+        animalError.innerText = "Name cannot be blank."
+        return
+    }
+
+    // Make sure the user put in a type
+    let type = animalForm.elements['animalType'].value
+    if (!type) {
+        animalError.innerText = "Type cannot be blank."
+        return
+    }
+
+    // Make sure the user put in an age
+    let age = Number(animalForm.elements['animalAge'].value)
+    if (!age || age <= 0) {
+        animalError.innerText = "Age cannot be blank and must be greater than 0."
+        return
+    }
+
+    // Make sure the user put in a gender
+    let gender = animalForm.elements['animalGender'].value
+    if (!gender) {
+        animalError.innerText = "Gender cannot be blank."
+        return
+    }
+
+    // Make sure the user put in a weight
+    let weight = Number(animalForm.elements['animalWeight'].value)
+    if (!weight || weight <= 0) {
+        animalError.innerText = "Weight cannot be blank and must be greater than 0."
+        return
+    }
+
+    // No need for validation as a checkbox can only have two states
+    let isPregnant = animalForm.elements['animalPregnant'].checked
+
+    let animal = {
+        name: name,
+        type: type,
+        age: age,
+        gender: gender,
+        weight: weight,
+        isPregnant: isPregnant
+    }
+
+    if (animalForm.elements['animalIndex'].value) {
+        let index = Number(animalForm.elements['animalIndex'].value)
+
+        editZooAnimal(index, animal)
+        animalForm.reset()
+    }
+    else {
+        addZooAnimal(animal)
+    }
+
+    animalForm.elements['animalIndex'].value = ""
+    animalError.innerText = ""
+});
+
+// Fill in existing values into edit form
+function populateEditForm(index) {
+    let animal = zoo.animals[index]
+    animalForm.elements['animalIndex'].value = index.toString()
+    animalForm.elements['animalName'].value = animal.name
+    animalForm.elements['animalType'].value = animal.type
+    animalForm.elements['animalAge'].value = animal.age
+    animalForm.elements['animalGender'].value = animal.gender
+    animalForm.elements['animalWeight'].value = animal.weight
+    animalForm.elements['animalPregnant'].checked = animal.isPregnant
+}
